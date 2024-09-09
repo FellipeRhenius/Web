fe8482efb835a3fced71836bb4e0f8603fafcbed
Fellipe <liperhenius@hotmail.com>
2024-09-09 19:20:00 -0300

    Script Update

--- HTML5/scripts/script-shop.js
+++ HTML5/scripts/script-shop.js
@@ -1,31 +1,30 @@
-function redirect(destination){
-    const routes = {
-        firstPage: "firstPage.html",
-        htmlPage: "htmlPage.html",
-        cssPage: "cssPage.html",
-        jsPage: "jsPage.html",
-        logicPage: "logicPage.html"
-    };
-    
-    if (routes[destination]){
-        window.location.href = routes[destination];
-    }
-    else{
-        console.error('Destino não encontrado!')
+function operation(type, product) {
+    const qtd = document.getElementById('qtd_' + product);
+    const value = document.getElementById('value_' + product);
+    const total = document.getElementById('total_' + product);
+
+    if (type == 'sub' && qtd.innerHTML == 0) {
+        alert('Menor impossivel')
+    } else {
+        type == 'add' ? qtd.innerHTML++ : qtd.innerHTML--
+        const totalValue = (qtd.innerHTML * onlyNumber(value.innerHTML)).toFixed(2)
+        total.innerHTML = valueFormater(totalValue)
+        sum()
     }
 }
-document.getElementById("btnFirst").addEventListener("click", function(){
-    redirect("firstPage");
-});
-document.getElementById("btnHtml").addEventListener("click", function(){
-    redirect("htmlPage");
-});
-document.getElementById("btnCss").addEventListener("click", function(){
-    redirect("cssPage");
-});
-document.getElementById("btnJs").addEventListener("click", function(){
-    redirect("jsPage");
-});
-document.getElementById("btnLogic").addEventListener("click", function(){
-    redirect("logicPage");
-});
+function sum() {
+    let sum = 0;
+
+    for(let i = 1;i<7;i++){
+        let number =onlyNumber(document.getElementById('total_' + i).innerHTML)
+        sum += Number(number)
+    }
+
+    document.getElementById('subtotal').innerHTML = sum.toFixed(2)
+}
+function onlyNumber(n){
+    return n.replace(/\D/g, '')
+}
+function valueFormater(n){
+    return 'R$ ' + n.toLocaeString('pt-BR')
+}
